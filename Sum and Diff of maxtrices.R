5194099ad6c64a3ee58b2bdb61fc26aefe5ad12b
CerkezVeyronix <143129200+CerkezVeyronix@users.noreply.github.com>
2023-09-30 11:20:58 -0400

    Add files via upload
    Shows sum and diff of two unique matrices

---
+++ Sum and Diff of maxtrices.R
@@ -0,0 +1,15 @@
+# Define matrices A and B
+A <- matrix(c(2, 0, 1, 3), ncol = 2)
+B <- matrix(c(5, 2, 4, -1), ncol = 2)
+
+# Find the sum A + B
+sum_result <- A + B
+
+# Find the difference A - B
+diff_result <- A - B
+
+# Print the results
+cat("Sum (A + B):\n")
+print(sum_result)
+cat("Difference (A - B):\n")
+print(diff_result)
